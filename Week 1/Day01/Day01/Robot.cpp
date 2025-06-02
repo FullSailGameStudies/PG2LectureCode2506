1298d9987d5294b67666517c38d57119dcfad93e
Garrett Girod <girodg@hotmail.com>
2025-06-02 12:08:13 -0400

    looping

--- Week 1/Day01/Day01/Robot.cpp
+++ Week 1/Day01/Day01/Robot.cpp
@@ -1,5 +1,6 @@
 #include "Robot.h"
 #include <iostream>
+#include <Console.h>
 
 
 //class method definitions
@@ -17,7 +18,56 @@ void Robot::CleanRoom(std::string roomToClean)
 
 std::string Robot::CookMeal(std::string recipe, std::vector<std::string> ingredients)
 {
-	std::cout << "Cooking the " << recipe << "...";
+	//how to call a Static method...
+	//1) use the name of the class prefixed to the method name.
+	Console::Write("Cooking the ");
+	Console::Write(recipe, ConsoleColor::Green);
+	//2) if it returns data, collect it
+	int width = Console::GetWindowWidth();
+
+	//std::cout << "Cooking the " << recipe << "...";
+	//for loop
+	std::cout << "\nFor loop...\n";
+	for (int i = 0; i < ingredients.size(); i++)
+	{
+		if(ingredients[i] == "Cinnamon")
+		{
+			auto cinnamonIterator = ingredients.begin() + i;
+			ingredients.erase(cinnamonIterator);
+		}
+		std::string ingredient = ingredients[i];
+		Console::WriteLine(ingredient);
+	}
+
+	std::cout << "\nForeach (range-based) loop...\n";
+	for (auto& ingredient : ingredients)
+	{
+		Console::WriteLine(ingredient);
+	}
+
+	std::cout << "\niterator for loop...\n";
+	//iterators are objects that "point" to the data
+	//  inside of a collection.
+	//they know how to move through a collection.
+	//vector.begin() - iterator to the first item in the vector
+	//vector.end() - iterator to the last item??
+	for (std::vector<std::string>::iterator i = ingredients.begin(); i != ingredients.end(); i++)
+	{
+		if (*i == "cinnamon")
+		{
+			ingredients.erase(i);//erase using the loop iterator
+		}
+		std::string ingredient = *i;//* - "dereference"
+		std::cout << ingredient << "\n";
+	}
+
+	//how to erase from a vector...
+	auto lastIterator = ingredients.end() - 1;
+	//to get an iterator to an item inside the vector...
+	//begin() + index
+	auto cinnamonIterator = ingredients.begin() + 2;
+	ingredients.erase(cinnamonIterator);
+
 	std::cout << "vrrr....\n";
 	std::cout << "DONE!\n";
 	return recipe;
