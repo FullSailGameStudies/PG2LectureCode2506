f4f206a7ca76a668af2467c4099746d78d964726
Garrett Girod <girodg@hotmail.com>
2025-06-23 12:05:20 -0400

    day 11

--- Week 4/Day11/Day11/Car.cpp
+++ Week 4/Day11/Day11/Car.cpp
@@ -4,3 +4,20 @@ std::string Car::vehicleInformation()
 {
 	return std::to_string(mModelYear) + " " + mMake + " " + mModel;
 }
+
+void Car::DeserializeCSV(const std::string& csvData, char delimiter)
+{
+	//use stringstream to parse the string
+	std::stringstream carStream(csvData);
+	std::string data;
+	std::getline(carStream, data, delimiter);
+	mModelYear = std::stoi(data);
+	std::getline(carStream, mMake, delimiter);
+	std::getline(carStream, mModel, delimiter);
+}
+
+void Car::SerializeCSV(std::ostream& outFile, char delimiter) const
+{
+	//  <<  insertion operator
+	outFile << mModelYear << delimiter << mMake << delimiter << mModel;
+}
--- Week 4/Day11/Day11/Day11.cpp
+++ Week 4/Day11/Day11/Day11.cpp
@@ -12,6 +12,19 @@ int main()
 {
     std::cout << "Gone in 60 Seconds!\n";//https://carbuzz.com/features/beyond-eleanor-gone-in-60-seconds-car-names#:~:text=The%20Complete%20List%20Of%20Gone%20In%2060%20Seconds,8%201961%20Porsche%20Speedster%20-%20%22Natalie%22%20More%20items
 
+    Car myRide(1969, "Plymouth", "Hemi Cuda");
+    std::string garageFile = "garage.csv";
+    std::ofstream outFile(garageFile);
+    if (outFile.is_open())
+    {
+        myRide.SerializeCSV(outFile, '~');
+    }
+    else std::cout << "The file couldn't be opened.\n";
+
+    outFile.close();
+
+    myRide.SerializeCSV(std::cout, '\t');
+    std::cout << "\n";
 
     std::vector<Car> garage;
     garage.push_back(Car(1967, "Ford", "Shelby Mustang GT500"));
@@ -20,7 +33,48 @@ int main()
     garage.push_back(Car(1965, "Pontiac", "GTO"));
     garage.push_back(Car(1969, "Plymouth", "Hemi Cuda"));
 
+    //1) serialize the vector to a file
+    std::string jaysGarage = "jaysGarage.csv";
 
+    std::ofstream jaysFile(jaysGarage);
+    if (jaysFile.is_open())
+    {
+        for(int i=0;i<garage.size();i++)
+        {
+            if(i != 0) jaysFile << "\n";
+            garage[i].SerializeCSV(jaysFile, '~');
+        }
+    }
+    else std::cout << "The file couldn't be opened.\n";
+    jaysFile.close();
+
+
+    std::vector<Car> loadedCars;
+
+    std::ifstream loadFile(jaysGarage);
+    if (loadFile.is_open())
+    {
+        //create a Car object using the data from the file
+        while(not loadFile.eof())
+        {
+            std::string carLine;
+            std::getline(loadFile, carLine);
+            if(not carLine.empty())
+            {
+                try
+                {
+                    Car nextCar(carLine, '~');
+                    loadedCars.push_back(nextCar);
+                }
+                catch (const std::exception&)
+                {
+                    std::cout << "Error reading car.\n";
+                }
+            }
+        }
+    }
+    else std::cout << jaysGarage << " could not be opened.\n";
+    loadFile.close();
     /*
         ╔═════════════╗
         ║ Serializing ║
@@ -51,4 +105,23 @@ int main()
             split each car to get the car details
 
     */
+
+    std::ifstream inFile(garageFile);
+    if (inFile.is_open())
+    {
+        //create a Car object using the data from the file
+        std::string carLine;
+        std::getline(inFile, carLine);
+        Car nextCar(carLine, '~');
+        nextCar.SerializeCSV(std::cout, '\t');
+        std::cout << "\n";
+    }
+    else std::cout << garageFile << " could not be opened.\n";
+    inFile.close();
+
+    std::string myCarInfo = "1967,Ford,Shelby Mustang GT500";
+    Car dreamCar(myCarInfo, ',');
+    dreamCar.SerializeCSV(std::cout, '\t');
+    std::cout << "\n";
+
 }
